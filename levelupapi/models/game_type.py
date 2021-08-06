802a65133439723af7aa149915cc1b8803c4e368
shawnbarron16 <shawnbarron16@gmail.com>
2021-08-06 10:15:36 -0500

    First Commit

---
+++ levelupapi/models/game_type.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class GameType(models.Model):
+    """GameType model
+    fields:
+        label (CharField): name of the game type
+    """
+    label = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.label
---
+++ levelupapi/views/__init__.py
@@ -0,0 +1,5 @@
+from .auth import login_user
+from .auth import register_user
+from .game import GameView
+from .game_type import GameTypeView
+from .event import EventView
